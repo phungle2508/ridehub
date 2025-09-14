0fde544eef2cc620a9d67bdea7f5e24c1b528236
phungvip2508 <shegga9x@gmail.com>
2025-09-14 10:48:15 +0700

    rm gateway

--- backend/gateway/src/main/webapp/app/entities/enumerations/booking-status.model.ts
+++
@@ -1,9 +0,0 @@
-export enum BookingStatus {
-  PENDING = 'PENDING',
-
-  CONFIRMED = 'CONFIRMED',
-
-  CANCELLED = 'CANCELLED',
-
-  COMPLETED = 'COMPLETED',
-}
--- backend/gateway/src/main/webapp/app/entities/ms_user/keycloak-user/keycloak-user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface IKeycloakUser {
-  id: string;
-  username?: string | null;
-  email?: string | null;
-  realmId?: string | null;
-  userId?: string | null;
-}
-
-export type NewKeycloakUser = Omit<IKeycloakUser, 'id'> & { id: null };
