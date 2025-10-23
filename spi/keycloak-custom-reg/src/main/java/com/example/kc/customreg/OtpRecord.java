28df4c63937ef25dc51f9ec4aad700b462f6e8f5
LeTrungMiniuh <trungmin0898@gmail.com>
2025-10-23 19:48:39 +0700

    Replace keycloak-custom-reg submodule with new repository keycloak-custom-reg-maian3333

--- spi/keycloak-custom-reg/src/main/java/com/example/kc/customreg/OtpRecord.java
+++
@@ -1,7 +0,0 @@
-package com.example.kc.customreg;
-
-import org.keycloak.common.util.Time;
-
-public record OtpRecord(String phone, String code, int expiresAt) {
-  public boolean isExpired() { return Time.currentTime() > expiresAt; }
-}
